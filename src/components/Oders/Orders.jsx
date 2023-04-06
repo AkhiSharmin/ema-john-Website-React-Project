9cf008f831ef97246ff0ffb192bfb5500878692d
AkhiSharmin <akhisharmin100@gmail.com>
2023-04-06 10:29:37 +0600

    orders page done

---
+++ src/components/Oders/Orders.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Cart from '../Cart/Cart';
+import { useLoaderData } from 'react-router-dom';
+
+const Orders = () => {
+
+    const products = useLoaderData();
+    console.log(products);
+
+    return (
+        <div className='shop-container'>
+           <div className='product-container'>
+                <h2>Orders Page: {products.length}</h2>
+           </div>
+           <div className='cart-container'>
+                <Cart cart={[]}></Cart>
+           </div>
+        </div>
+    );
+};
+
+export default Orders;
